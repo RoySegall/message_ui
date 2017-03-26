cb539f97ac6e7a588a18751d1df3ec80f1b8eccb
RoySegall <roy.segall@gizra.com>
2017-03-26 20:58:50 +0200

    Commit more work.

--- modules/message_notify_ui/src/Form/MessageNotifyForm.php
+++ modules/message_notify_ui/src/Form/MessageNotifyForm.php
@@ -11,9 +11,9 @@ use Drupal\Core\Entity\EntityForm;
 use Drupal\Core\Entity\EntityManagerInterface;
 use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\message_notify\MessageNotifier;
-use Drupal\message_notify_ui\Plugin\MessageNotifyUiSenderSettingsFormInterface;
-use Drupal\message_notify_ui\Plugin\MessageNotifyUiSenderSettingsFormManager;
+use Drupal\message_notify_ui\MessageNotifyUiSenderSettingsFormManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Component\Datetime\TimeInterface;
 use Drupal\message_notify\Plugin\Notifier\Manager;
@@ -70,9 +70,23 @@ class MessageNotifyForm extends EntityForm {
   /**
    * Message notify UI sender settings form manager.
    *
-   * @var \Drupal\message_notify_ui\Plugin\MessageNotifyUiSenderSettingsFormInterface
+   * @var \Drupal\message_notify_ui\MessageNotifyUiSenderSettingsFormInterface
    */
   protected $messageNotifyUiSenderSettingsForm;
+
+  /**
+   * The language manager service.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
+
+  /**
+   * Keep track of the notify plugins and their matching sender pluign ID.
+   *
+   * @var array
+   */
+  protected $plugins;
 
   /**
    * Constructs a ContentEntityForm object.
@@ -87,9 +101,10 @@ class MessageNotifyForm extends EntityForm {
    *   The message notifier service.
    * @param \Drupal\message_notify\Plugin\Notifier\Manager $message_notify_manager
    *   Message notifier manager service.
-   * @param \Drupal\message_notify_ui\Plugin\MessageNotifyUiSenderSettingsFormManager $message_notify_ui_setting_form_manager
+   * @param \Drupal\message_notify_ui\MessageNotifyUiSenderSettingsFormManager $message_notify_ui_setting_form_manager
    *   The message notify UI sender settings form manager.
-   *
+   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
+   *   The language manager interface.
    */
   public function __construct(
     EntityManagerInterface $entity_manager,
@@ -97,7 +112,8 @@ class MessageNotifyForm extends EntityForm {
     TimeInterface $time = NULL,
     MessageNotifier $message_notifier,
     Manager $message_notify_manager,
-    MessageNotifyUiSenderSettingsFormManager $message_notify_ui_setting_form_manager)
+    MessageNotifyUiSenderSettingsFormManager $message_notify_ui_setting_form_manager,
+    LanguageManagerInterface $language_manager)
   {
     $this->entityManager = $entity_manager;
 
@@ -106,6 +122,7 @@ class MessageNotifyForm extends EntityForm {
     $this->messageNotifier = $message_notifier;
     $this->messageNotifierManager = $message_notify_manager;
     $this->messageNotifyUiSenderSettingsForm = $message_notify_ui_setting_form_manager;
+    $this->languageManager = $language_manager;
   }
 
   /**
@@ -118,7 +135,8 @@ class MessageNotifyForm extends EntityForm {
       $container->get('datetime.time'),
       $container->get('message_notify.sender'),
       $container->get('plugin.message_notify.notifier.manager'),
-      $container->get('plugin.manager.message_notify_ui_sender_settings_form')
+      $container->get('plugin.manager.message_notify_ui_sender_settings_form'),
+      $container->get('language_manager')
     );
   }
 
@@ -133,14 +151,63 @@ class MessageNotifyForm extends EntityForm {
       $senders[$definition['id']] = $definition['title'];
     }
 
+    $senders_ids = array_keys($senders);
+
+
     $form['senders'] = [
       '#type' => 'radios',
       '#title' => $this->t('Select a notifier handler'),
       '#options' => $senders,
+      '#default_value' => reset($senders_ids),
     ];
 
-    dpm($this->messageNotifyUiSenderSettingsForm->getDefinitions());
+    $form['senders_form'] = [];
+
+    if ($this->languageManager->isMultilingual()) {
+      // Multilingual is on. Add languages to the select list.
+      $languages = [];
+
+      foreach ($this->languageManager->getLanguages() as $language) {
+        $languages[$language->getId()] = $language->getName();
+      }
+
+      $form['language'] = [
+        '#type' => 'select',
+        '#title' => $this->t('Select a language'),
+        '#options' => $languages,
+      ];
+    }
+
+    foreach ($this->messageNotifyUiSenderSettingsForm->getDefinitions() as $definition) {
+      $plugin = $this->messageNotifyUiSenderSettingsForm->createInstance($definition['id']);
+      $this->plugins[$definition['plugin']] = $definition['id'];
+
+      $form['senders_form'][$definition['id']] = $plugin->form();
+    }
 
     return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $this
+      ->messageNotifyUiSenderSettingsForm
+      ->createInstance($this->plugins[$form_state->getValue('senders')])
+      ->setMessage($this->entity)
+      ->validate($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $plugin = $form_state->getValue('senders');
+    $this
+      ->messageNotifyUiSenderSettingsForm
+      ->createInstance($this->plugins[$plugin])
+      ->setMessage($this->entity)
+      ->submit($this->messageNotifier, $form_state);
   }
 }
--- modules/message_notify_ui/src/Plugin/MessageNotifyUiSenderSettingsForm/MessageNotifyUiSenderMailSettingsForm.php
+++ modules/message_notify_ui/src/Plugin/MessageNotifyUiSenderSettingsForm/MessageNotifyUiSenderMailSettingsForm.php
@@ -2,8 +2,11 @@
 
 namespace Drupal\message_notify_ui\Plugin\MessageNotifyUiSenderSettingsForm;
 
-use Drupal\message_notify_ui\Plugin\MessageNotifyUiSenderSettingsFormBase;
-use Drupal\message_notify_ui\Plugin\MessageNotifyUiSenderSettingsFormInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\message_notify\MessageNotifier;
+use Drupal\message_notify\Plugin\Notifier\MessageNotifierBase;
+use Drupal\message_notify_ui\MessageNotifyUiSenderSettingsFormBase;
+use Drupal\message_notify_ui\MessageNotifyUiSenderSettingsFormInterface;
 
 /**
  * @MessageNotifyUiSenderSettingsForm(
@@ -18,12 +21,45 @@ class MessageNotifyUiSenderMailSettingsForm extends MessageNotifyUiSenderSetting
    * {@inheritdoc}
    */
   public function form() {
+    return [
+      'use_custom' => [
+        '#type' => 'checkbox',
+        '#title' => t('Use custom email'),
+        '#description' => t('Use the message owner message'),
+      ],
+      'email' => [
+        '#type' => 'email',
+        '#title' => t('Email address'),
+        '#description' => t('The email address'),
+        '#states' => [
+          'visible' => [
+            ':input[name="use_custom"]' => ['checked' => TRUE],
+          ],
+        ],
+      ],
+    ];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function submit() {
+  public function validate($form, FormStateInterface $formState) {
+    if ($formState->getValue('use_custom') && !$formState->getValue('email')) {
+      $formState->setErrorByName('email', t('The email field cannot be empty.'));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submit(MessageNotifier $notifier, FormStateInterface $formState) {
+    $settings = [];
+
+    if ($formState->getValue('use_custom')) {
+      $settings['mail'] = $formState->getValue('email');
+    }
+
+    $notifier->send($this->getMessage(), $settings, 'email');
   }
 
 }
