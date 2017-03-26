a7b93dddc57cad9591bcf19f1d867c8066f1dbc5
RoySegall <roy.segall@gizra.com>
2017-03-26 15:10:32 +0200

    Adding notify integration.

---
+++ modules/message_notify_ui/src/Form/MessageNotifyForm.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\message_ui\Form\MessageDeleteForm.
+ */
+
+namespace Drupal\message_notify_ui\Form;
+
+use Drupal\Core\Entity\ContentEntityConfirmFormBase;
+use Drupal\Core\Entity\ContentEntityForm;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
+/**
+ * Provides a form for deleting a message_ui entity.
+ *
+ * @ingroup message_notify_ui
+ */
+class MessageNotifyForm extends ContentEntityForm {
+
+}
